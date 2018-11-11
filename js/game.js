a2a4fab205d3623c9f94a432fc47af3db4262579
space2pacman <space2pacman@gmail.com>
2018-11-11 17:41:56 +0300

    modified

--- js/game.js
+++ js/game.js
@@ -1,58 +1,100 @@
 var doc = document;
-var objects = doc.querySelector(".objects");
 var room = doc.querySelector(".room");
 var field = doc.querySelector(".field");
 var wall;
 var money;
 var needles;
-var obstacle;
 var finish;
 var water;
+
 var size = {
 	w: 50,
 	h: 50
 };
-var arrow = {
+
+var key = {
 	up: 38,
 	left: 37,
 	right: 39
 };
+
 var id = {
-	empty: 0,
-	wall: 1,
-	money: 2,
-	needles: 3,
-	player: 4,
-	enemy: 5,
-	obstacle: 6,
-	finish: 7,
-	surface: 8,
-	water: 9
+	empty: 100,
+	wall: {
+		default: 101,
+		surface: 103,
+		stone: 106,
+		box: 107
+	},
+	finish: 102,
+	water: {
+		default: 104,
+		surface: 105
+	},
+	money: 200,
+	obstacle: 300,
+	enemy: {
+		bee: 301,
+		bat: 302,
+		fish: 303
+	},
+	needles: 400,
+	player: 500,
 };
+
 var template = {
-	wall: "<div class='wall'></div>",
-	empty: "<div class='empty'></div>",
-	money: "<div class='money'></div>",
-	needles: "<div class='needles'></div>",
-	player: "<div class='player'></div>",
-	enemy: "<div class='enemy'></div>",
-	obstacle: "<div class='obstacle'></div>",
-	finish: "<div class='finish'></div>",
-	surface: "<div class='wall surface'></div>",
-	water: "<div class='water'></div>"
+	wall: {
+		default: "wall",
+		surface: "wall-surface",
+		stone: "wall-stone",
+		box: "wall-box"
+	},
+	empty: "empty",
+	money: "money",
+	needles: "needles",
+	player: "player",
+	enemy: {
+		default: "enemy",
+		bee: "bee",
+		bat: "bat",
+		fish: "fish"
+	},
+	obstacle: "obstacle",
+	finish: "finish",
+	water: {
+		default: "water",
+		surface: "water-surface"
+	}
 };
-var map = [
-	[0,0,8,8,6,0,5,0,0,0,6,1,0,0,0,0,0,0,0,0],
-	[0,0,0,1,6,5,0,0,0,0,6,1,0,0,0,0,0,0,0,0],
-	[0,0,0,1,6,0,0,0,5,0,6,1,0,0,0,0,0,0,0,0],
-	[4,0,8,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,6,0,0,0,6,0,0,0,0,0,0,0,8,0,0,0,0,0],
-	[0,0,6,0,0,0,6,0,0,0,0,0,0,8,1,8,0,0,0,0],
-	[8,8,8,8,8,8,8,8,9,9,9,9,8,1,1,1,0,0,0,0],
-	[1,1,1,1,1,1,1,1,8,8,8,8,1,1,1,1,8,8,8,8]
-];
+
+var map = {
+	length: 24,
+	objects: [
+		[100,100,107,107,100,100,100,100,100,100,100,107,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,107,100,100,100,100,100,100,100,107,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,107,100,100,100,100,100,100,100,107,100,100,100,100,100,100,100,100,100,100,100,100],
+		[500,100,107,107,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,300,100,100,100,300,100,100,100,100,100,100,106,106,106,100,100,100,100,100,100,100,100],
+		[100,100,300,100,100,100,300,100,100,100,100,100,100,103,103,103,105,105,105,105,105,105,105,103],
+		[103,103,103,103,103,103,103,103,105,105,105,105,103,101,101,101,104,104,104,104,104,104,104,101],
+		[101,101,101,101,101,101,101,101,103,103,103,103,101,101,101,101,103,103,103,103,103,103,103,101]
+	],
+	enemies:[
+		[100,100,100,100,300,100,302,100,100,100,300,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,300,301,100,100,100,100,300,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,300,100,100,100,301,100,300,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,300,100,303,100,100,100,300,100],
+		[100,100,100,100,100,100,100,100,300,303,100,300,100,100,100,100,300,100,100,100,100,303,300,100],
+		[100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100]
+	]
+};
+
 var player = {
 	el: null,
 	x: 0,
@@ -63,6 +105,7 @@ var player = {
 		gravity: 2,
 		speed: 1
 	},
+	range: 50,
 	jumpHeight: 0,
 	jumpMaxHeight: 10,
 	setPosition: function() {
@@ -70,19 +113,13 @@ var player = {
 		this.el.style.top = this.y + "px";
 	},
 	checkPosition: function() {
-		/*if(objects.offsetLeft + objects.offsetWidth < field.offsetWidth) {
-			console.log("stop world")
-			world.move = false;
-			world.x = -500;
-			objects.style.left = world.x + "px";
-		}*/
 		if((player.el.offsetLeft + player.el.offsetWidth > field.offsetWidth / 2
-			&& objects.offsetLeft + objects.offsetWidth > field.offsetWidth)
+			&& objects.el.offsetLeft + objects.el.offsetWidth > field.offsetWidth)
 			|| (player.el.offsetLeft + player.el.offsetWidth < field.offsetWidth / 2
-				&& objects.offsetLeft != 0)) {
-			world.move = true;
+				&& objects.el.offsetLeft != 0)) {
+			world.freeze = false;
 		} else {
-			world.move = false;
+			world.freeze = true;
 		};
 		//console.log("p: " + (player.el.offsetLeft + player.el.offsetWidth) + " f " + field.offsetWidth / 2 )
 	},
@@ -97,11 +134,11 @@ var player = {
 	places: function() {
 		room.innerHTML = "";
 
-		for (var i = 0; i < map.length; i++) {
-			for(var j = 0; j < map[i].length; j++) {
-				if(map[i][j] == id.player) {
-					room.innerHTML += template.player;
-					objects.innerHTML += template.empty;
+		for (var i = 0; i < map.objects.length; i++) {
+			for(var j = 0; j < map.objects[i].length; j++) {
+				if(map.objects[i][j] == id.player) {
+					room.appendChild(objects.create(template.player));
+					objects.el.appendChild(objects.create(template.empty));
 					this.x = j * size.w;
 					this.y = i * size.h;
 				};
@@ -123,6 +160,7 @@ var player = {
 		collision();
 	}
 };
+
 var enemy = {
 	el: null,
 	x: [],
@@ -150,149 +188,220 @@ var enemy = {
 		}
 	},
 	switchResolution: function(index, resolution) {
-		for(var i = 0; i < obstacle.length; i++) {
-			if(this.x[index] + this.el[index].offsetWidth == obstacle[i].offsetLeft + obstacle[i].offsetWidth
-				&& this.x[index] == obstacle[i].offsetLeft
-				&& this.y[index] + this.el[index].offsetHeight == obstacle[i].offsetTop + obstacle[i].offsetHeight
-				&& this.y[index] == obstacle[i].offsetTop) {
+		for(var i = 0; i < obstacle.el.length; i++) {
+			if(this.x[index] + this.el[index].offsetWidth == obstacle.el[i].offsetLeft + obstacle.el[i].offsetWidth
+				&& this.x[index] == obstacle.el[i].offsetLeft
+				&& this.y[index] + this.el[index].offsetHeight == obstacle.el[i].offsetTop + obstacle.el[i].offsetHeight
+				&& this.y[index] == obstacle.el[i].offsetTop) {
 				this.resolution[index] = resolution;
 			};
 		};
 	}
 };
+
 var world = {
 	x: 0,
-	move: false,
+	freeze: true,
 	speed: 10,
 	setWidth: function() {
-		objects.style.width = map[0].length * size.w + "px";
+		objects.el.style.width = map.objects[0].length * size.w + "px";
 	},
 	setPosition: function(value) {
 		this.x = value;
-		objects.style.left = this.x + "px";
+		objects.el.style.left = this.x + "px";
 	},
 	reset: function() {
 		this.setPosition(0);
-		this.move = false;
-		objects.style.left = this.x + "px";
+		this.freeze = true;
+		objects.el.style.left = this.x + "px";
+	},
+	move: function(direction) {
+		var index;
+
+		if(direction == "right") index = -1;
+		if(direction == "left") index = 1;
+
+		if(!this.freeze && this.x <= 0) {
+			this.x += player.step.walk * player.step.speed * index;
+			objects.el.style.left = this.x + "px";
+
+			checkCollision(wall, function() {
+				this.x -= player.step.walk * player.step.speed * index;
+				objects.el.style.left = this.x + "px";
+			}.bind(this))	
+		} else {
+			player.move(direction);
+		};
+
+		if(this.x > 0) {
+			player.move(direction);
+			this.reset();
+		};
 	}
 };
 
-function placesObjects() {
-	objects.innerHTML = "";
+var obstacle = {
+	el: null,
+	x: [],
+	y: [],
+	places: function() {
+		for(var i = 0; i < this.el.length; i++) {
+			this.el[i].style.left = this.x[i] + "px";
+			this.el[i].style.top = this.y[i] + "px";
+		}
+	}
+};
 
-	for (var i = 0; i < map.length; i++) {
-		for(var j = 0; j < map[i].length; j++) {
-			switch(map[i][j]) {
-				case id.empty:
-					objects.innerHTML += template.empty;
-					break;
-				case id.wall:
-					objects.innerHTML += template.wall;
-					break;
-				case id.money:
-					objects.innerHTML += template.money;
-					break;
-				case id.needles:
-					objects.innerHTML += template.needles;
-					break;
-				case id.player:
-					player.places();
-					break;
-				case id.enemy:
-					objects.innerHTML += template.enemy;
-					objects.innerHTML += template.empty;
-					enemy.x.push(j * size.w);
-					enemy.y.push(i * size.h);
-					enemy.resolution.push(true);
-					break;
-				case id.obstacle:
-					objects.innerHTML += template.obstacle;
-					break;
-				case id.finish:
-					objects.innerHTML += template.finish;
-					break;
-				case id.surface:
-					objects.innerHTML += template.surface;
-					break;
-				case id.water:
-					objects.innerHTML += template.water;
-					break;
+var objects = {
+	el: doc.querySelector(".objects"),
+	places: function(type) {
+
+		for (var i = 0; i < map[type].length; i++) {
+			for(var j = 0; j < map[type][i].length; j++) {
+				switch(map[type][i][j]) {
+					case id.empty:
+						objects.el.appendChild(objects.create(template.empty));
+						break;
+					case id.wall.default:
+						objects.el.appendChild(objects.create(template.wall.default));
+						break;
+					case id.money:
+						objects.el.appendChild(objects.create(template.money));
+						break;
+					case id.needles:
+						objects.el.appendChild(objects.create(template.needles));
+						break;
+					case id.player:
+						player.places();
+						break;
+					case id.enemy.bee:
+						var obj = objects.create(template.enemy.default)
+						obj.classList.add(template.enemy.bee);
+						objects.el.appendChild(obj);
+						objects.el.appendChild(objects.create(template.empty));
+						enemy.x.push(j * size.w);
+						enemy.y.push(i * size.h);
+						enemy.resolution.push(true);
+						break;
+					case id.enemy.bat:
+						var obj = objects.create(template.enemy.default)
+						obj.classList.add(template.enemy.bat);
+						objects.el.appendChild(obj);
+						objects.el.appendChild(objects.create(template.empty));
+						enemy.x.push(j * size.w);
+						enemy.y.push(i * size.h);
+						enemy.resolution.push(true);
+						break;
+					case id.enemy.fish:
+						var obj = objects.create(template.enemy.default)
+						obj.classList.add(template.enemy.fish);
+						objects.el.appendChild(obj);
+						objects.el.appendChild(objects.create(template.empty));
+						enemy.x.push(j * size.w);
+						enemy.y.push(i * size.h);
+						enemy.resolution.push(true);
+						break;
+					case id.obstacle:
+						objects.el.appendChild(objects.create(template.obstacle));
+						objects.el.appendChild(objects.create(template.empty));
+						obstacle.x.push(j * size.w);
+						obstacle.y.push(i * size.h);
+						break;
+					case id.finish:
+						objects.el.appendChild(objects.create(template.finish));
+						break;
+					case id.wall.surface:
+						var obj = objects.create(template.wall.default)
+						obj.classList.add(template.wall.surface);
+						objects.el.appendChild(obj);
+						break;
+					case id.wall.stone:
+						var obj = objects.create(template.wall.default)
+						obj.classList.add(template.wall.stone);
+						objects.el.appendChild(obj);
+						break;
+					case id.wall.box:
+						var obj = objects.create(template.wall.default)
+						obj.classList.add(template.wall.box);
+						objects.el.appendChild(obj);
+						break;
+					case id.water.default:
+						objects.el.appendChild(objects.create(template.water.default));
+						break;
+					case id.water.surface:
+						var obj = objects.create(template.water.default)
+						obj.classList.add(template.water.surface);
+						objects.el.appendChild(obj);
+						break;
+				};
 			};
 		};
-	};
+	},
+	find: function() {
+		wall = doc.querySelectorAll(".wall");
+		money = doc.querySelectorAll(".money");
+		needles = doc.querySelectorAll(".needles");
+		player.el = doc.querySelector(".player");
+		finish = doc.querySelectorAll(".finish");
+		enemy.el = doc.querySelectorAll(".enemy");
+		obstacle.el = doc.querySelectorAll(".obstacle");
+		objects.el = doc.querySelector(".objects");
+		water = doc.querySelectorAll(".water");
+	},
+	create: function(type) {
+		var el = doc.createElement("div");
+		el.classList.add(type);
+		return el;
+	}
 };
 
-function findObjects() {
-	wall = doc.querySelectorAll(".wall");
-	money = doc.querySelectorAll(".money");
-	needles = doc.querySelectorAll(".needles");
-	player.el = doc.querySelector(".player");
-	finish = doc.querySelectorAll(".finish");
-	enemy.el = doc.querySelectorAll(".enemy");
-	obstacle = doc.querySelectorAll(".obstacle");
-	water = doc.querySelectorAll(".water");
+var game = {
+	move: function(e) {
+		switch(e.keyCode) {
+			case key.up:
+				player.jump();
+				break;
+			case key.left:
+				world.move("left");
+				player.el.classList.remove("left")
+				player.el.classList.add("right");
+				break;
+			case key.right:
+				world.move("right");
+				player.el.classList.remove("right")
+				player.el.classList.add("left");
+				break;
+		};
+
+		collision();
+	},
+	gameover: function() {
+		player.places();
+		objects.find();
+		player.setPosition();
+		world.reset();
+	},
+	init: function() {
+		objects.places("objects");
+		objects.places("enemies");
+		objects.find();
+		obstacle.places();
+		setInterval(player.gravity, world.speed);
+		setInterval(enemy.move.bind(enemy), world.speed * enemy.speed);
+		player.setPosition();
+		world.setWidth();
+		doc.addEventListener("keydown", game.move);
+	}
 };
 
-function move(e) {
-	switch(e.keyCode) {
-		case arrow.up:
-			player.jump();
-			break;
-		case arrow.left:
-			moveWorld("left");
-			player.el.classList.remove("left")
-			player.el.classList.add("right");
-			break;
-		case arrow.right:
-			moveWorld("right");
-			player.el.classList.remove("right")
-			player.el.classList.add("left");
-			break;
-	};
-
-	collision();
-};
-
-function gameover() {
-	player.places();
-	findObjects();
-	player.setPosition();
-	world.reset();
-};
-
-function checkCollision(object, action) {
+function checkCollision(object, action, range = 0) {
 	for(var i = 0; i < object.length; i++) {
 		if(player.x - world.x + player.el.offsetWidth > object[i].offsetLeft
 			&& player.x - world.x < object[i].offsetLeft + object[i].offsetWidth
-			&& player.y + player.el.offsetHeight > object[i].offsetTop
+			&& player.y + player.el.offsetHeight > object[i].offsetTop + range
 			&& player.y < object[i].offsetTop + object[i].offsetHeight) {
 			action(i);
 		};
-	};
-};
-
-function moveWorld(direction) {
-	var index;
-
-	if(direction == "right") index = -1;
-	if(direction == "left") index = 1;
-
-	if(world.move && world.x <= 0) {
-		world.x += player.step.walk * player.step.speed * index;
-		objects.style.left = world.x + "px";
-
-		checkCollision(wall, function() {
-			world.x -= player.step.walk * player.step.speed * index;
-			objects.style.left = world.x + "px";
-		})	
-	} else {
-		player.move(direction);
-	};
-
-	if(world.x > 0) {
-		player.move(direction);
-		world.reset();
 	};
 };
 
@@ -316,23 +425,13 @@ function collision() {
 	player.step.speed = 1;
 	checkCollision(water, function() {
 		player.step.speed = 0.5;
-	});
+	},player.range);
 
 	checkCollision(wall, player.freeze.bind(player));
-	checkCollision(needles, gameover);
-	checkCollision(enemy.el, gameover);
+	checkCollision(needles, game.gameover);
+	checkCollision(enemy.el, game.gameover);
 	player.setPosition();
 	player.checkPosition();
-}
+};
 
-function init() {
-	placesObjects();
-	findObjects();
-	setInterval(player.gravity, world.speed);
-	setInterval(enemy.move.bind(enemy), world.speed * enemy.speed);
-	player.setPosition();
-	world.setWidth();
-	doc.addEventListener("keydown", move);
-}
-
-init();
+game.init();
